b8e1f29fa2205682957a378ff4bb3d85198f3111
Océane Vignot <82438041+Incitis@users.noreply.github.com>
2023-05-03 12:50:41 +0200

    /command en cours, besoin de débug

--- Events/ready.js
+++ Events/ready.js
@@ -1,5 +1,8 @@
 const Discord = require("discord.js");
+const loadSlashCommand = require("../Loaders/LoadSlashCommands");
 
 module.exports = async (bot) => {
+  await loadSlashCommand(bot);
+
   console.log(`${bot.user.tag} is online.`);
 };
